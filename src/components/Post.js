a86c1d5be0918a134a2472ce00c0885a3ab0ec34
Peter Springer <peterspringer829@gmail.com>
2016-10-23 14:53:04 -0600

    testing post request

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Post extends Component {
+  constructor() {
+  super();
+  this.state = {
+    title: '',
+  };
+}
+
+  handleTitleValue(e) {
+      this.setState({ title: e.target.value })
+    }
+
+    sendPostToListing() {
+      axios.post('http://localhost:8080/api/v1/listing', {
+      title: 'does this work???',
+      price: '5',
+      description: 'test post request',
+      user_id: '1',
+      category_id: '1',
+    })
+    .then(function () {
+      console.log("this worked!");
+    })
+    .catch(function () {
+      console.log("ah shitt");
+    });
+  }
+
   render() {
     return (
       <div className="post">
         <h1>MAKE A POST</h1>
+        <input
+          placeholder="title"
+          onChange={(e)=>this.handleTitleValue(e)}
+          />
+        <button
+          onClick={()=>this.sendPostToListing()}
+          >submit</button>
       </div>
     );
   }
